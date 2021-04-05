79d60e0d7a0840a0d0f986ef9242ecfedaa8a266
Juan Leon <spidre.gold@gmail.com>
2021-04-05 18:27:01 -0500

    added sum and substract methods

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -4,5 +4,15 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		System.out.println("Calculator");
+		System.out.println("Sum: (2 + 3) = " + sum(2,3));
+		System.out.println("Substract: (2 + 3) = " + substract(2,3));
+	}
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	public static int substract(int a, int b) {
+		return a - b;
 	}
 }
